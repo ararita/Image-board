94cac7d1425573112445154235aefe832c138c48
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-22 16:43:06 +0100

    uploadFile function sorted out, functional

--- db.js
+++ db.js
@@ -10,3 +10,11 @@ if (process.env.DATABASE_URL) {
 module.exports.getImages = () => {
     return db.query(`SELECT * FROM images`);
 };
+
+module.exports.addImage = function(url, username, title, description) {
+    return db.query(
+        `INSERT INTO images (url, username, title, description)
+        VALUES ($1, $2, $3, $4) RETURNING *`,
+        [url, username, title, description]
+    );
+};
